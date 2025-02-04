503cd2205e17aff91c9591d4f60037266e4f441a
EDAH Kodjo <elomedah@gmail.com>
2025-02-04 23:32:03 +0100

    Step-1

---
+++ src/main/java/com/learn/kafka/consumer/MessageConsumer.java
@@ -0,0 +1,17 @@
+package com.learn.kafka.consumer;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MessageConsumer {
+
+    @KafkaListener(topics = "xxxxx", groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(String message) {
+        log.info("Message receive : {}", message);
+    }
+
+}
